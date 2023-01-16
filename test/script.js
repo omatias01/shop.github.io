bef5f26ee19b4ffcdbdcd011780b010fbdfd22f1
Mateus Barbosa <matias>
2023-01-15 23:25:05 -0300

    teste falhou tirei tudo

--- test/script.js
+++ test/script.js
@@ -4,9 +4,7 @@ import SimplexNoise from "https://cdn.skypack.dev/simplex-noise@3.0.0";
 import hsl from "https://cdn.skypack.dev/hsl-to-hex";
 import debounce from "https://cdn.skypack.dev/debounce";
 
-function loading(){
-  $('#load').css('display','none');
-}
+
 // return a random number within a range
 function random(min, max) {
   return Math.random() * (max - min) + min;
