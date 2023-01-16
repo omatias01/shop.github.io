a63cc7f123cf648af21c7f33b3ad9e50cea63738
Mateus Barbosa <matias>
2023-01-15 23:17:34 -0300

    teste carregamento

--- test/script.js
+++ test/script.js
@@ -4,6 +4,9 @@ import SimplexNoise from "https://cdn.skypack.dev/simplex-noise@3.0.0";
 import hsl from "https://cdn.skypack.dev/hsl-to-hex";
 import debounce from "https://cdn.skypack.dev/debounce";
 
+function loading(){
+  $('#load').css('display','none');
+}
 // return a random number within a range
 function random(min, max) {
   return Math.random() * (max - min) + min;
